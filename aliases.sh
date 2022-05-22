c8d91fbe47978db995da2e0b16d05974aa8c3d5c
Maciej Bielski <bielski.maciek@gmail.com>
2022-05-22 22:57:46 +0200

    interactive ripgrep
    Signed-off-by: Maciej Bielski <bielski.maciek@gmail.com>

--- aliases.sh
+++ aliases.sh
@@ -27,3 +27,28 @@ ndiff () {
     nvim -d ${_other_root}/${_rel_fp} $2
 }
 
+# interactive grep
+# TESTING: od -vAn -tcx1
+irg () {
+
+    _get_fpath() { echo "${1:-''}" | cut -d':' -f1 }
+    _get_linenb() {  echo "${1:-''}" | cut -d':' -f2 }
+
+    # if [ -n "$1" ]; then
+        RG_INC='-g"**/*.[chi]" -g"**/*.py" -g"**/*.py3" -g"**/*.hh" -g"**/*.cc" -g"**/*.cpp" -g"**/*.hpp" -g"**/*.bb" -g"**/*.bbappend" -g"**/*.sh" -g"**/*.txt" -g"**/*.md"'
+        fname_ln=$( \
+            RG_PREFIX="rg --with-filename -n --no-heading --no-messages --color=ansi --smart-case ${RG_INC}"
+            INITIAL_QUERY="${1:- }"
+            FZF_DEFAULT_COMMAND="$RG_PREFIX ''" TERM='tmux-256color' \
+                fzf --ansi --query "$INITIAL_QUERY" \
+                --delimiter=':' \
+                --preview="nvim -u $HOME/.config/nvim/init_pager.vim -c':e {1}' -c ':{2}' -c ':norm zz' -c ':redraw' " \
+                 --preview-window='up:75%' --sync) \
+                     && _fpath=$(_get_fpath "${fname_ln}") \
+                     && _line_nb=$(_get_linenb "${fname_ln}") \
+                     && nvim ${_fpath} +${_line_nb} -c ':norm zz'
+                     # && vifm -c "!nvim ${_fpath} +${_line_nb} -c ':norm zz'"
+    # else
+    #     false
+    # fi
+}
