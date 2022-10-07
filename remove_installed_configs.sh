042f3796513909f301f43dcd637307b4604107ee
Maciej Bielski <bielski.maciek@gmail.com>
2022-10-07 17:14:56 +0200

    add cleanup script
    Signed-off-by: Maciej Bielski <bielski.maciek@gmail.com>

---
+++ remove_installed_configs.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/bash
+
+function rmFile() {
+    printf "$(tput setaf 1; tput setab 3)[!] Removing: $1$(tput sgr 0)\n\n"
+    rm -f "$1"
+}
+
+declare -r SCRIPT_DIR=$(readlink -f $(dirname -- "${BASH_SOURCE[0]}"))
+
+pushd "$SCRIPT_DIR"
+rmFile '~/.tmux.conf'
+rmFile '~/.config/vifm/vifmrc'
+rmFile '~/.config/vifm/vifm_wrapper.sh'
+rmFile '~/.config/vifm/colors/Default.vifm'
+
+sed -i '/~\/.config\/vifm\/vifm_wrapper.sh/ d' ~/.zshrc
+
+rmFile '~/.config/nvim/init.vim'
+rmFile '~/.config/nvim/init_pager.vim'
+
+rmFile '~/.config/nvim/after/syntax/markdown.vim'
+
+rmFile '~/.config/env_vars.sh'
+sed -i '/~\/.config\/env_vars.sh/ d' ~/.zshrc
+
+rmFile '~/.config/aliases.sh'
+sed -i '/~\/.config\/aliases.sh/ d' ~/.zshrc
+
+rmFile '~/.gitconfig_inc'
+
+rmFile '~/.fzf.zsh'
+sed -i '/~\/.fzf.zsh/ d' ~/.zshrc
+
+rmFile '~/.zshenv'
+
+popd
