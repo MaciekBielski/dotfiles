57c7ff3cd28d054f4c35a445e06af292228b85a9
Maciej Bielski <bielski.maciek@gmail.com>
2023-01-06 13:29:07 +0100

    align with sles script
    Signed-off-by: Maciej Bielski <bielski.maciek@gmail.com>

--- installer_zsh_ubuntu.sh
+++ installer_zsh_ubuntu.sh
@@ -14,25 +14,30 @@ function sourceFirstFromSecond() {
 
 declare -r SCRIPT_DIR=$(readlink -f $(dirname -- "${BASH_SOURCE[0]}"))
 
-sudo apt update
-sudo apt -y install git tmux vifm cscope fzf xclip ripgrep neovim curl
-sudo apt -y install zsh-syntax-highlighting
+# sudo apt update
+# sudo apt -y install git tmux vifm cscope fzf xclip ripgrep neovim curl
+# sudo apt -y install zsh-syntax-highlighting
 
 pushd "$SCRIPT_DIR"
 install -m 0644 -T ./tmux.conf ~/.tmux.conf
 install -m 0644 -T -D ./vifmrc ~/.config/vifm/vifmrc
-install -m 0644 -T -D ./vifm_wrapper.sh ~/.config/vifm/vifm_wrapper.sh
+# install -m 0644 -T -D ./vifm_wrapper.sh ~/.config/vifm/vifm_wrapper.sh
 install -m 0644 -T -D ./Default.vifm ~/.config/vifm/colors/Default.vifm
 
 sourceFirstFromSecond '~/.config/vifm/vifm_wrapper.sh' ~/.zshrc
 
-sh -c "curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
+# sh -c "curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
 install -m 0644 -T ./init.vim ~/.config/nvim/init.vim
 install -m 0644 -T ./functions.vim ~/.config/nvim/functions.vim
+install -m 0644 -T -D ./opts.lua ~/.config/nvim/lua/opts.lua
+install -m 0644 -T -D ./keymap.lua ~/.config/nvim/lua/keymap.lua
+install -m 0644 -T -D ./autocmds.lua ~/.config/nvim/lua/autocmds.lua
 install -m 0644 -T ./init_pager.vim ~/.config/nvim/init_pager.vim
 
 install -m 0644 -T -D ./markdown.vim ~/.config/nvim/after/syntax/markdown.vim
 nvim -c 'PlugInstall' -c 'qall'
+~/.config/nvim/autoload/fzf/install --key-bindings --completion --no-fish --no-update-rc
+sourceFirstFromSecond '~/.fzf.zsh' ~/.zshrc
 
 install -m 0644 -T -D ./env_vars.sh ~/.config/env_vars.sh
 sourceFirstFromSecond '~/.config/env_vars.sh' ~/.zshrc
@@ -41,9 +46,6 @@ install -m 0644 -T -D ./aliases.sh ~/.config/aliases.sh
 sourceFirstFromSecond '~/.config/aliases.sh' ~/.zshrc
 
 install -m 0644 -T -D ./gitconfig_inc ~/.gitconfig_inc
-
-install -m 0644 -T ./fzf.zsh ~/.fzf.zsh
-sourceFirstFromSecond '~/.fzf.zsh' ~/.zshrc
 
 install -m 0644 -T ./zshenv ~/.zshenv
 sourceFirstFromSecond '/usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh' ~/.zshenv
